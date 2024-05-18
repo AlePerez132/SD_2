60344c6e196e083cec8dcc7ff87623d493c49f5e
Jiuso <jesusgg200x@gmail.com>
2024-05-18 16:53:49 +0200

    Buscar medio funcionando (pendiente hacer case insensitive al contains).

--- src/gestorbibliotecacliente/GestorBibliotecaCliente.java
+++ src/gestorbibliotecacliente/GestorBibliotecaCliente.java
@@ -7,9 +7,6 @@ package gestorbibliotecacliente;
 import gestorbibliotecacomun.GestorBibliotecaIntf;
 import gestorbibliotecacomun.TDatosRepositorio;
 import java.util.Scanner;
-import java.rmi.RemoteException;
-import java.rmi.server.*;
-import java.rmi.registry.*;
 import java.rmi.Naming;
 import gestorbibliotecacomun.TLibro;
 
@@ -55,6 +52,7 @@ public class GestorBibliotecaCliente {
         TDatosRepositorio repositorio = null;
         int repositorioElegido = 0;
         int numeroLibrosInicial = 0;
+        boolean punteroAlgunaCoincidencia[] = {false, false, false, false, false}; // Array de booleanos para la búsqueda en todos los campos (*).
 
         //Declaramos variables result como en la práctica de C:
         int result_1 = -1;
@@ -64,7 +62,7 @@ public class GestorBibliotecaCliente {
         int result_5 = -1;
         int result_6 = -1;
         int result_7 = -1;
-        int result_8 = -1;
+        boolean result_8 = false;
         int result_9 = -1;
         int result_10 = -1;
         TLibro result_11 = null;
@@ -221,11 +219,190 @@ public class GestorBibliotecaCliente {
                                                 confirmacionCompra = scChar.next().charAt(0);
                                                 if (confirmacionCompra != 's') { // Si el usuario no ha confirmado con s:
                                                     System.out.print("*** Compra abortada ***\n");
+                                                } else { // Si el usuario ha confirmado con s:
+                                                    System.out.print("Introduce Numero de Libros comprados:\n");
+                                                    numeroLibrosComprados = scInt.nextInt();
+                                                    // Pasamos los parámetros:
+                                                    result_6 = GestorStub.Comprar(idAdministrador, isbnCompra, numeroLibrosComprados);
+                                                    if (result_6 == -1) {
+                                                        System.out.print("ERROR: ya hay un administrador logueado\n");
+                                                    } else if (result_6 == 0) {
+                                                        System.out.print("ERROR: no se ha encontrado el libro\n");
+                                                    } else if (result_6 == 1) {
+                                                        System.out.print("*** Se han agregado los nuevos ejemplares del libro y los datos están ordenados ***\n");
+                                                    }
                                                 }
                                             }
                                         }
                                         break;
                                     }
+                                    case 5: {//Retirar libro.
+                                        System.out.print("Introduce Isbn a Buscar:\n");
+                                        isbnCompra = scString.nextLine();
+                                        // Por ISBN.
+                                        result_10 = GestorStub.Buscar(idAdministrador, isbnCompra);
+                                        if (result_10 == -2) {
+                                            System.out.print("ERROR: ya hay un administrador logueado");
+                                        } else if (result_10 == -1) {
+                                            System.out.println("No se ha encontrado ningun libro");
+                                        } else { //mostrar libro
+                                            result_11 = GestorStub.Descargar(idAdministrador, -1, result_10 + 1);
+                                            if (result_11 != null) {
+                                                // Hemos recibido el resultado bien, podemos guardarlo en libro y escribir por pantalla.
+                                                libro = result_11;
+                                                System.out.print((result_10 + 1) + "\t" + libro.getTitulo() + "\t" + libro.getIsbn() + "\t" + libro.getNoLibros() + "\t" + libro.getNoListaEspera() + "\n");
+                                                System.out.print(libro.getAutor() + "\t" + libro.getPais() + "\t" + libro.getIdioma() + "\t" + libro.getAnio() + "\n");
+                                            }
+                                        }
+                                        System.out.println("Es este el libro que quieres retirar(s/n)?");
+                                        confirmacionCompra = scChar.next().charAt(0);
+                                        if (confirmacionCompra == 's') {
+                                            System.out.println("Introduce el numero de unidades a retirar:");
+                                            numeroLibrosRetirados = scInt.nextInt();
+                                            result_7 = GestorStub.Retirar(idAdministrador, isbnCompra, numeroLibrosRetirados);
+                                            if (result_7 == -1) {
+                                                System.out.print("ERROR: ya hay un administrador logueado");
+                                            } else if (result_7 == 0) {
+                                                System.out.print("ERROR: no se ha encontrado ningun libro");
+                                            } else if (result_7 == 2) {
+                                                System.out.println("No hay suficientes ejemplares para retirar");
+                                            } else if (result_7 == 1) {
+                                                System.out.println("Se han reducido el número de ejemplares disponibles y se han ordenado los datos");
+                                            }
+                                        }
+                                        break;
+                                    }
+                                    case 6: {//Ordenar.
+                                        System.out.println("0.- Por Isbn\n");
+                                        System.out.println("1.- Por Titulo\n");
+                                        System.out.println("2.- Por Autor\n");
+                                        System.out.println("3.- Por Anho\n");
+                                        System.out.println("4.- Por Pais\n");
+                                        System.out.println("5.- Por Idioma\n");
+                                        System.out.println("6.- Por nº de libros Disponibles\n");
+                                        System.out.println("7.- Por nº de libros Prestados\n");
+                                        System.out.println("8.- Por nº de libros en Espera\n");
+                                        System.out.println("Elige el campo que ordenara los libros:\n");
+                                        campoElegido = scInt.nextInt();
+                                        // Llamamos a ordenar en el servidor:
+                                        result_8 = GestorStub.Ordenar(idAdministrador, campoElegido);
+                                        if (campoElegido >= 0 && campoElegido <= 8) {
+                                            if (!result_8) {
+                                                System.out.println("ERROR: el id administrador no coincide con el del servidor\n");
+                                            } else if (result_8) {
+                                                System.out.println("Se ha ordenado correctamente el vector\n");
+                                            }
+                                        }
+                                        break;
+                                    }
+                                    case 7: {//Buscar.
+                                        System.out.print("Introduce el texto a buscar:\n");
+                                        textoABuscar = scString.nextLine();
+                                        System.out.print("I.- Por Isbn\n");
+                                        System.out.print("T.- Por Titulo\n");
+                                        System.out.print("A.- Por Autor\n");
+                                        System.out.print("P.- Por Pais\n");
+                                        System.out.print("D.- Por Idioma\n");
+                                        System.out.print("*.- Por todos los campos\n");
+                                        System.out.print("Introduce el codigo de busqueda\n");
+                                        codigoBusqueda = scChar.next().charAt(0);
+                                        for (int i = 1; i <= numeroRepositorios; i++) {
+                                            repositorio = GestorStub.DatosRepositorio(idAdministrador, i);
+                                            if (repositorio != null) {
+                                                System.out.println(i + "\t" + repositorio.getNombre() + "\t" + repositorio.getDireccion() + "\t" + repositorio.getNumLibros());
+                                            }
+                                        }
+                                        result_9 = GestorStub.NLibros(-1); // Recogemos el nº de libros del servidor para todos los repos.
+                                        System.out.println("0\tTodos los repositorios\t\t" + result_9);
+                                        System.out.println("Elige repositorio:");
+                                        repositorioElegido = scInt.nextInt();
+                                        if (repositorioElegido == 0) {
+                                            repositorioElegido = -1;//El servidor entiende todos los repos como -1, el cliente lo ve como 0. De ahí esta condición y asignación.
+                                        }
+                                        //Descargaremos cada libro y filtraremos:
+                                        result_9 = GestorStub.NLibros(repositorioElegido); // Recogemos el nº de libros del servidor.
+                                        if (result_9 == -1) {
+                                            System.out.print("ERROR: el repositorio no existe\n");
+                                        } else {
+                                            System.out.print("POS\tTITULO\tISBN\tDIS\tPRE\tPOS\n");
+                                            System.out.print("\tAUTOR\tPAIS (IDIOMA)\tANIO\n");
+                                            System.out.print("*********************************************************************************************\n");
+
+                                            NumLibros = result_9;
+
+                                            // Descargaremos cada libro del servidor. Si pasa el filtrado, lo mostraremos por pantalla:
+                                            for (int i = 0; i < NumLibros; i++) {
+                                                result_11 = GestorStub.Descargar(idAdministrador, repositorioElegido, i);
+                                                if (result_11 != null) {
+                                                    libro = result_11;// Hemos recibido el resultado bien, podemos guardarlo en libro.
+                                                    // Solo mostraremos el libro si aparece la cadena buscada en los campos deseados.
+                                                    // Usaremos contains.
+                                                    // Para la búsqueda monocampo, emplearemos el puntero respectivo: punteroAlgunaCoincidencia[i]. i valdrá lo que diga el mapa: {Isbn:0, Titulo:1, Autor:2, Pais:3, Idioma:4}.
+                                                    // Para la búsqueda multicampo (*), emplearemos todo el array de punteros: punteroAlgunaCoincidencia.
+                                                    switch (codigoBusqueda) {
+                                                        case 'I': {
+                                                            punteroAlgunaCoincidencia[0] = libro.getIsbn().contains(textoABuscar); // Isbn:0.
+                                                            punteroAlgunaCoincidencia[1] = false;
+                                                            punteroAlgunaCoincidencia[2] = false;
+                                                            punteroAlgunaCoincidencia[3] = false;
+                                                            punteroAlgunaCoincidencia[4] = false;
+                                                            break;
+                                                        }
+                                                        case 'T': {
+                                                            punteroAlgunaCoincidencia[1] = libro.getTitulo().contains(textoABuscar); // Titulo:1.
+                                                            // Inicializo el resto de punteros a false:
+                                                            punteroAlgunaCoincidencia[0] = false;
+                                                            punteroAlgunaCoincidencia[2] = false;
+                                                            punteroAlgunaCoincidencia[3] = false;
+                                                            punteroAlgunaCoincidencia[4] = false;
+                                                            break;
+                                                        }
+                                                        case 'A': {
+                                                            punteroAlgunaCoincidencia[2] = libro.getAutor().contains(textoABuscar); // Autor:2.
+                                                            // Inicializo el resto de punteros a false:
+                                                            punteroAlgunaCoincidencia[0] = false;
+                                                            punteroAlgunaCoincidencia[1] = false;
+                                                            punteroAlgunaCoincidencia[3] = false;
+                                                            punteroAlgunaCoincidencia[4] = false;
+                                                            break;
+                                                        }
+                                                        case 'P': {
+                                                            punteroAlgunaCoincidencia[3] = libro.getPais().contains(textoABuscar); // Pais:3.
+                                                            // Inicializo el resto de punteros a false:
+                                                            punteroAlgunaCoincidencia[0] = false;
+                                                            punteroAlgunaCoincidencia[1] = false;
+                                                            punteroAlgunaCoincidencia[2] = false;
+                                                            punteroAlgunaCoincidencia[4] = false;
+                                                            break;
+                                                        }
+                                                        case 'D': {
+                                                            punteroAlgunaCoincidencia[4] = libro.getIdioma().contains(textoABuscar); // Idioma:4.
+                                                            // Inicializo el resto de punteros a false:
+                                                            punteroAlgunaCoincidencia[0] = false;
+                                                            punteroAlgunaCoincidencia[1] = false;
+                                                            punteroAlgunaCoincidencia[2] = false;
+                                                            punteroAlgunaCoincidencia[3] = false;
+                                                            break;
+                                                        }
+                                                        case '*': {
+                                                            punteroAlgunaCoincidencia[0] = libro.getIsbn().contains(textoABuscar); // Isbn:1.
+                                                            punteroAlgunaCoincidencia[1] = libro.getTitulo().contains(textoABuscar); // Titulo:1.
+                                                            punteroAlgunaCoincidencia[2] = libro.getAutor().contains(textoABuscar); // Autor:2.
+                                                            punteroAlgunaCoincidencia[3] = libro.getPais().contains(textoABuscar); // Pais:3.
+                                                            punteroAlgunaCoincidencia[4] = libro.getIdioma().contains(textoABuscar); // Idioma:4.
+                                                            break;
+                                                        }
+                                                    }
+                                                    if (punteroAlgunaCoincidencia[0] != false || punteroAlgunaCoincidencia[1] != false || punteroAlgunaCoincidencia[2] != false || punteroAlgunaCoincidencia[3] != false || punteroAlgunaCoincidencia[4] != false) {
+                                                        System.out.print(i + "\t" + libro.getTitulo() + "\t" + libro.getIsbn() + "\t" + libro.getNoLibros() + "\t" + libro.getNoListaEspera() + "\n");
+                                                        System.out.print(libro.getAutor() + "\t" + libro.getPais() + "\t" + libro.getIdioma() + "\t" + libro.getAnio() + "\n");
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        break;
+                                    }
+
                                     case 8: { //Listar libro
                                         // Recogemos del servidor el numero de libros:
                                         result_9 = GestorStub.NLibros(-1); // Recogemos el nº de libros del servidor (todos los repositorios con *).
@@ -237,7 +414,7 @@ public class GestorBibliotecaCliente {
                                             System.out.print("*********************************************************************************************\n");
 
                                             NumLibros = result_9;
-                                                //MODIFICADO TEMPORALMENTE PARA ACCEDER AL PRIMER REPOSITORIO
+                                            //MODIFICADO TEMPORALMENTE PARA ACCEDER AL PRIMER REPOSITORIO
                                             for (int i = 1; i <= NumLibros; i++) {
                                                 result_11 = GestorStub.Descargar(idAdministrador, -1, i);
                                                 if (result_11 != null) {
